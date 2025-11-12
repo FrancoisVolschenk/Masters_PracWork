f319910429df05b5e3c9a6cf949f052a5e91b01a
FrancoisVolschenk <francoisv934@gmail.com>
2025-11-12 16:38:25 +0200

    Fixed typo in dataset loading

--- Fingerprint_Synthesis/train_VAE.py
+++ Fingerprint_Synthesis/train_VAE.py
@@ -68,7 +68,7 @@ def load_data(batch_size):
     print("Loading the training dataset")
 
     dataset = ImageFolder(
-        root=f"{BASE_PATH}dataset/light_bg",
+        root=f"{BASE_PATH}/dataset/light_bg",
         transform=transform,
     )
     train_loader = DataLoader(
